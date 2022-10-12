b7d75e1ad12d405d52e575ac008b755060e9c43c
Gardeniae <113418465+Gardeniae@users.noreply.github.com>
2022-10-12 17:53:04 +0800

    Update 数据生成器.py

--- 数据生成器.py
+++ 数据生成器.py
@@ -1,50 +1,35 @@
-import random
-import numpy as np
+import webbrowser
 import openpyxl
 
-def random_jidian():
-    jidian = np.random.normal(3.0, 0.5, 90)
-    for i in range(90):
-        while jidian[i]>4:jidian[i]=np.random.normal(3.0, 0.5, 1)
-        if jidian[i]<0:jidian[i]=0
+for ii in range(1,6):
+    wb = openpyxl.load_workbook('点名情况课程'+str(ii)+'.xlsx')
+    ws = wb['课程'+str(ii)]
 
-    jidian.sort()
-    return jidian
+    a = []
+    for i in range(1,91):
+        a.append((ws.cell(i+1,22).value*10,i))
+    a.sort()
 
-def random_attendance():
-    
-    taoke = []
-    taokeren = random.randint(7,8)
-
-    muqian = 0
-    for i in range(90):
-        if random.random()>0.5 :
-            taoke.append(i)
-            muqian+=1
-        if muqian==taokeren : break
-
-    cell = [[1 for _ in range(20)] for _ in range(90)]
-
-
-
-    for i in range(20):
-        suijitao = random.randint(0,3)
-        suiji = []
-        for j in range(suijitao):
-            suiji.append(random.randint(0,89))
-
-        for j in range(90):
-            if suiji.count(j)>0 :
-                cell[j][i]=0
-
-    for i in range(90):
-        if taoke.count(i)>0:
-            suiji = []
-            for j in range(2,20):suiji.append(j)
-            suijishan=random.sample(suiji,4)
-            for j in range(20):
-                if suijishan.count(j):cell[i][j]=1
-                else : cell[i][j]=0
-
-    
-    return cell 
+    x=0
+    y=0
+    k=30
+    mi=8
+    for i in range(1,21):
+        if i==1 : k=20
+        if i==2 : k=13
+        if i==3 : k=11
+        if i==4 : k=10
+        if i>4 : k=mi
+        print('第'+str(ii)+'门课第'+str(i)+'次课点:')
+        for j in range(k):
+            print(ws.cell(a[j][1]+1,1).value,end=" ")
+            y+=1
+            if ws.cell(a[j][1]+1,i+1).value==0 :
+                a[j]=(a[j][0]-50,a[j][1])
+                x+=1
+            if i>4 :
+                if a[j][0]>-100 :
+                    mi-=1
+        a.sort()
+        print(" ")
+    print("E值为",x/y)
